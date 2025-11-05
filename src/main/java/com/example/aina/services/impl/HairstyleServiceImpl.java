765f6349fd55a65afc197c5cb311d38093555f88
Zharaskhan <m_jasik_2005@mail.ru>
2025-11-06 01:13:12 +0600

    started creating the class impl, added method get all and some methods too

--- src/main/java/com/example/aina/services/impl/HairstyleServiceImpl.java
+++ src/main/java/com/example/aina/services/impl/HairstyleServiceImpl.java
@@ -1,15 +1,27 @@
 package com.example.aina.services.impl;
 
 import com.example.aina.dto.HairstyleDto;
+import com.example.aina.entities.Hairstyle;
+import com.example.aina.repository.HairstyleRepository;
 import com.example.aina.services.HairstyleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-
+@Service
+@RequiredArgsConstructor
 public class HairstyleServiceImpl implements HairstyleService {
+    private final HairstyleRepository hRep;
+
     @Override
     public List<HairstyleDto> getAllHairstyles() {
-        return List.of();
-    }
+        List<Hairstyle> hairstyles = hRep.findAll();
+        List<HairstyleDto> result = new ArrayList<>();
+        for (Hairstyle hair : hairstyles) {
+            result.add(convertToDto(hair));
+        }
+        return result;    }
 
     @Override
     public HairstyleDto getHairstyleById(Long id) {
@@ -39,5 +51,25 @@ public class HairstyleServiceImpl implements HairstyleService {
     @Override
     public HairstyleDto updateImageUrl(Long id, String imageUrl) {
         return null;
+    }
+
+    private HairstyleDto convertToDto(Hairstyle hair) {
+        HairstyleDto dto = new HairstyleDto();
+        dto.setId(hair.getId());
+        dto.setStyleName(hair.getStyleName());
+        dto.setFaceShape(hair.getFaceShape());
+        dto.setHairType(hair.getHairType());
+        dto.setImageUrl(hair.getImageUrl());
+        return dto;
+    }
+
+    private Hairstyle convertToEntity(HairstyleDto dto) {
+        Hairstyle hair = new Hairstyle();
+        hair.setId(dto.getId());
+        hair.setStyleName(dto.getStyleName());
+        hair.setFaceShape(dto.getFaceShape());
+        hair.setHairType(dto.getHairType());
+        hair.setImageUrl(dto.getImageUrl());
+        return hair;
     }
 }
